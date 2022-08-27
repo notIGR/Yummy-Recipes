3d13830d8c08f662a6028d2d449adf6c7dc24e2d
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-08-27 10:47:25 -0400

    connected api top web page, need to add data to table

--- src/pages/dinner/index.jsx
+++ src/pages/dinner/index.jsx
@@ -1,9 +1,45 @@
+import { useQuery } from "@tanstack/react-query"
+import { Recipe } from "../recipes/[id]";
 
-export default function DinnerList() {
+const DinnerList = () => {
+    const { isLoading, error, data } = useQuery(['allDinner'], () =>
+        fetch('/api/dinner').then(res =>
+            res.json()
+        )
+    );
+
+    if (isLoading) {
+        return (
+            <h1>Loading....</h1>
+        )
+    }
+
+    if (error) {
+        return (
+            <h1>{JSON.stringify(error, undefined, 2)}</h1>
+        )
+    }
+
+
     return (
         <>
-            <h1> Dinner Options </h1>
-            <h2>what to choose...</h2>
+            <h1> Dinner Recipe List </h1>
+            {
+                data.map((recipe) => {
+                    return (
+                        <Recipe
+                            key={recipe.id}
+                            title={recipe.title}
+                            type={recipe.type}
+                            ingredients={recipe.ingredients}
+                            instructions={recipe.instructions}
+                        />
+                    )
+                }
+                )
+            }
         </>
     )
 }
+
+export default DinnerList
